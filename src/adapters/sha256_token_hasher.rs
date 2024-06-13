09df0c7e7bcb55fbb7ffce873cff757081dd11f8
JKearnsl <boyarshinovn@gmail.com>
2024-06-14 01:23:52 +0300

    added sha2

---
+++ src/adapters/sha256_token_hasher.rs
@@ -0,0 +1,32 @@
+use argon2::password_hash::rand_core::OsRng;
+use argon2::password_hash::SaltString;
+use async_trait::async_trait;
+use sha2::{Digest, Sha256};
+
+use crate::application::common::hasher::Hasher;
+
+pub struct Sha256TokenHasher {}
+
+#[async_trait]
+impl Hasher for Sha256TokenHasher {
+    async fn hash(&self, value: &str) -> String {
+        let mut hasher = Sha256::new();
+        let value = value.to_owned();
+        let hash = tokio::task::spawn_blocking(move || {
+            hasher.update(value.as_bytes());
+            format!("{:x}", hasher.finalize())
+        }).await.unwrap();
+        hash
+    }
+
+    async fn verify(&self, value: &str, hash: &str) -> bool {
+        let mut hasher = Sha256::new();
+        let value = value.to_owned();
+        let hash = hash.to_owned();
+        let result = tokio::task::spawn_blocking(move || {
+            hasher.update(value.as_bytes());
+            format!("{:x}", hasher.finalize()) == hash
+        }).await.unwrap();
+        result
+    }
+}
