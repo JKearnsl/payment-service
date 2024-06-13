b74bce9a12dd2b868fce0e356e866290b3b869c1
JKearnsl <boyarshinovn@gmail.com>
2024-06-13 20:50:18 +0300

    initial

---
+++ src/application/token/create.rs
@@ -0,0 +1,37 @@
+use crate::application::common::exceptions::{ApplicationError, ErrorContent};
+use crate::application::common::hasher::Hasher;
+use crate::application::common::id_provider::IdProvider;
+use crate::application::common::interactor::Interactor;
+use crate::application::common::token_gateway::TokenGateway;
+use crate::domain::models::token::{Token, TokenKey};
+use crate::domain::services::validator::ValidatorService;
+
+pub struct CreateToken<'a> {
+    pub token_gateway: &'a dyn TokenGateway,
+    pub token_hasher: &'a dyn Hasher,
+    pub validator: &'a ValidatorService,
+    pub id_provider: Box<dyn IdProvider>,
+}
+
+impl Interactor<(), TokenKey> for CreateToken<'_> {
+    async fn execute(&self, data: ()) -> Result<TokenKey, ApplicationError> {
+        
+        if !self.id_provider.is_auth() {
+            return Err(ApplicationError::Unauthorized(
+                ErrorContent::Message("Необходимо авторизоваться".to_string())
+            ));
+        }
+        
+        let token_key = Token::generate_token_key();
+        let hashed_token = self.token_hasher.hash(&token_key).await;
+        
+        let model = Token::new(
+            self.id_provider.user_id().unwrap().clone(),
+            hashed_token,
+        );
+
+        self.token_gateway.save_token(&model).await;
+
+        Ok(token_key)
+    }
+}
---
+++ src/application/token/get_list.rs
@@ -0,0 +1,50 @@
+use chrono::{DateTime, Utc};
+use serde::Serialize;
+
+use crate::application::common::exceptions::{ApplicationError, ErrorContent};
+use crate::application::common::id_provider::IdProvider;
+use crate::application::common::interactor::Interactor;
+use crate::application::common::token_gateway::TokenReader;
+use crate::domain::models::token::{TokenHash, TokenId};
+use crate::domain::models::user::UserId;
+
+#[derive(Debug, Serialize)]
+pub struct TokenListItemDTO{
+    id: TokenId,
+    user_id: UserId,
+    hash: TokenHash,
+    created_at: DateTime<Utc>
+}
+
+
+pub type GetTokenListResultDTO = Vec<TokenListItemDTO>;
+
+
+pub struct GetTokenList<'a> {
+    pub token_reader: &'a dyn TokenReader,
+    pub id_provider: Box<dyn IdProvider>,
+}
+
+impl Interactor<(), GetTokenListResultDTO> for GetTokenList<'_> {
+    async fn execute(&self, data: ()) -> Result<GetTokenListResultDTO, ApplicationError> {
+        
+        if !self.id_provider.is_auth() {
+            return Err(
+                ApplicationError::Unauthorized(
+                    ErrorContent::Message("Необходимо авторизоваться".to_string())
+                )
+            );
+        }
+        
+        let tokens = self.token_reader.get_user_tokens(
+            self.id_provider.user_id().unwrap()
+        ).await;
+
+        Ok(tokens.into_iter().map(|t| TokenListItemDTO {
+            id: t.id,
+            user_id: t.user_id,
+            hash: t.hash,
+            created_at: t.created_at
+        }).collect())
+    }
+}
