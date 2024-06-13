bdbb93adf8138e43da623b3eb6bd8008104de126
JKearnsl <boyarshinovn@gmail.com>
2024-06-13 20:49:47 +0300

    removed

--- src/application/common/token_processor.rs
+++
@@ -1,8 +0,0 @@
-use crate::domain::models::session_token_hash::SessionTokenHash;
-use crate::domain::models::user::UserId;
-
-
-pub trait TokenProcessor {
-    fn validate_token(&self, token: &SessionTokenHash) -> Option<&UserId>;
-    fn set_token(&mut self, token: SessionTokenHash, user_id: UserId);
-}
