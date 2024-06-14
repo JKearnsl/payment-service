47e7fe9a1e1ce1162d0413a7564307c90b8c3993
JKearnsl <boyarshinovn@gmail.com>
2024-06-14 19:34:12 +0300

    added PartialEq

--- src/domain/models/payment_state.rs
+++ src/domain/models/payment_state.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use strum_macros::{Display, EnumIter};
 
-#[derive(Display, EnumIter, Clone, Debug, Deserialize, Serialize)]
+#[derive(Display, EnumIter, Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub enum PaymentState {
     Pending,
     Paid,
