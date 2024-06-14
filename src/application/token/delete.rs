97c903e12a874afd70e6e904818b430314ee8522
JKearnsl <boyarshinovn@gmail.com>
2024-06-14 03:19:50 +0300

    added delete

---
+++ src/application/token/delete.rs
@@ -0,0 +1,54 @@
+use serde::Deserialize;
+
+use crate::application::common::exceptions::{ApplicationError, ErrorContent};
+use crate::application::common::id_provider::IdProvider;
+use crate::application::common::interactor::Interactor;
+use crate::application::common::token_gateway::{TokenGateway, TokenReader};
+use crate::domain::models::token::TokenId;
+
+#[derive(Debug, Deserialize)]
+pub struct DeleteTokenDTO{
+    id: TokenId,
+}
+
+
+pub struct DeleteToken<'a> {
+    pub token_gateway: &'a dyn TokenGateway,
+    pub id_provider: Box<dyn IdProvider>,
+}
+
+impl Interactor<DeleteTokenDTO, ()> for DeleteToken<'_> {
+    async fn execute(&self, data: DeleteTokenDTO) -> Result<(), ApplicationError> {
+        
+        if !self.id_provider.is_auth() {
+            return Err(
+                ApplicationError::Unauthorized(
+                    ErrorContent::Message("Unauthorized".to_string())
+                )
+            );
+        }
+        
+        let token = match self.token_gateway.get_token(&data.id).await {
+            Some(token) => token,
+            None => {
+                return Err(
+                    ApplicationError::NotFound(
+                        ErrorContent::Message("Token not found".to_string())
+                    )
+                );
+            }
+        };
+
+        if token.user_id != *self.id_provider.user_id().unwrap() {
+            return Err(
+                ApplicationError::Forbidden(
+                    ErrorContent::Message("Forbidden".to_string())
+                )
+            );
+        }
+        
+        self.token_gateway.remove_token(&data.id).await;
+
+        Ok(())
+    }
+}
--- src/application/token/mod.rs
+++ src/application/token/mod.rs
@@ -1,2 +1,3 @@
 pub mod create;
 pub mod get_list;
+pub mod delete;
